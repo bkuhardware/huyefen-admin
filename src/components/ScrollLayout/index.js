288aed401cab9f3a94aba40f226d87f519a94449
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-04-18 10:44:34 +0700

    scroll layout

---
+++ src/components/ScrollLayout/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from 'react';
+import withRouter from 'umi/withRouter';
+import { Layout } from 'antd';
+import Scrollbars from 'react-custom-scrollbars';
+import styles from './index.less';
+
+const ScrollLayout = ({ children, location }) => {
+    const scrollElRef = useRef(null);
+    const { pathname } = location;
+    useEffect(() => {
+        const element = scrollElRef.current;
+        if (element) element.scrollToTop();
+    }, [pathname]);
+    return (
+        <Scrollbars
+            className={styles.container}
+            ref={scrollElRef}
+            style={{ height: 'auto' }}
+        >
+            <Layout>
+                {children}
+            </Layout>
+        </Scrollbars>
+    )
+};
+
+export default withRouter(ScrollLayout);
